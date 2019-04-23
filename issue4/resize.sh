196fd54154f76cb1cd38f14aba3c9b47a6e5fa15
Eric Bréchemier <eric@bezen.org>
2019-04-23 18:13:59 +0200

    Extend script to resize files in extracts/ as well

--- issue4/resize.sh
+++ issue4/resize.sh
@@ -1,22 +1,30 @@
 #!/bin/sh
-# Resize all PNG images from large/ into small/, preserving 8-bit density
+# Resize all PNG images from large/ and extracts/ into small/,
+# preserving 8-bit density
 #
 # Requires: convert (from ImageMagick)
 #
 cd "$(dirname "$0")"
 
+resize()
+{
+  for folder in ??-*
+  do
+    echo "Create folder small/$folder..."
+    mkdir -p "../small/$folder"
+  done
+
+  for file in ??-*/*.png
+  do
+    echo "Create file small/$file..."
+    convert "$file" -resize 50% -depth 8 "../small/$file"
+  done
+}
+
 cd large/
+resize
 
-for folder in ??-*
-do
-  echo "Create folder small/$folder..."
-  mkdir -p "../small/$folder"
-done
-
-for file in ??-*/*.png
-do
-  echo "Create file small/$file..."
-  convert "$file" -resize 50% -depth 8 "../small/$file"
-done
+cd ../extracts
+resize
 
 echo "Done."
